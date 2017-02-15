5f6f19a6b95f9f70b419737c1bda8bf184ab0f00
Balázs Buri <info@burib.net>
2017-02-15 17:28:21 +0100

    set content type manually

--- server.py
+++ server.py
@@ -6,24 +6,33 @@ def hello():
 
 @bottle.post('/POST')
 def hello():
+    bottle.request.content_type = 'application/json'
     bottle.response.content_type = 'application/json'
 
-    return {
+    responseData = {
         "data": {
             "json": bottle.request.json,
             "bodyRead": bottle.request.body.read()
         }
     }
+
+    print(responseData)
+
+    return responseData
 
 @bottle.delete('/DELETE')
 def hello():
     bottle.response.content_type = 'application/json'
     
-    return {
+    responseData = {
         "data": {
             "json": bottle.request.json,
             "bodyRead": bottle.request.body.read()
         }
     }
+
+    print(responseData)
+
+    return responseData
 
 bottle.run(host='localhost', port=8080, debug=True)
